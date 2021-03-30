4755a5dc359f9504371b0c9c8702ce1d176f3512
catman0745 <catman0745@gmail.com>
2021-03-30 18:40:51 +0300

    Add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import { Switch, Route } from 'react-router-dom'
 import { HomePage, SignInPage } from '../pages'
 
+import Header from './layout/Header'
+
 const App = () => (
   <div className="app">
+    <Header />
+
     <Switch>
       <Route path="/home" render={HomePage} />
       <Route path="/" render={SignInPage} />
---
+++ src/components/layout/Header.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+const Header = () => (
+  <Navbar className="shadow-sm border-bottom" bg="light">
+    <Navbar.Brand as={Link} to="/home">
+      Catman.Education
+    </Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link as={Link} to="/home">
+        Home
+      </Nav.Link>
+      <Nav.Link as={Link} to="/">
+        Sign in
+      </Nav.Link>
+    </Nav>
+  </Navbar>
+)
+
+export default Header
