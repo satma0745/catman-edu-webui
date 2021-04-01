9e8b2adb3b2063096dbf7ac26642c1c607017cf1
catman0745 <catman0745@gmail.com>
2021-04-01 14:50:00 +0300

    Add dropdown common control

--- src/components/common/controls/index.js
+++ src/components/common/controls/index.js
@@ -1,4 +1,5 @@
+import Dropdown from './Dropdown'
 import Input from './Input'
 import PasswordInput from './PasswordInput'
 
-export { Input, PasswordInput }
+export { Dropdown, Input, PasswordInput }
