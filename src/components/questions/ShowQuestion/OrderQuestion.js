e5a2468c043244b26ec00758fb7ec7b9e6bcee8c
catman0745 <catman0745@gmail.com>
2021-04-16 09:08:54 +0300

    Remove console.log statement from ShowQuestion/OrderQuestion

--- src/components/questions/ShowQuestion/OrderQuestion.js
+++ src/components/questions/ShowQuestion/OrderQuestion.js
@@ -1,23 +1,19 @@
 import ListGroup from 'react-bootstrap/ListGroup'
 import QuestionTemplate from './QuestionTemplate'
 
-const OrderQuestion = ({ cost, text, items = [], onEdit: edit, onDelete: $delete }) => {
-  console.log({ items })
-
-  return (
-    <QuestionTemplate cost={cost} text={text} onEdit={edit} onDelete={$delete}>
-      <ListGroup variant="flush">
-        {items
-          .sort((left, right) => left.orderIndex - right.orderIndex)
-          .map(({ id, text: optionText }, index) => (
-            <ListGroup.Item key={id}>
-              <span className="text-primary">{index + 1}.</span>
-              <span className="ml-1">{optionText}</span>
-            </ListGroup.Item>
-          ))}
-      </ListGroup>
-    </QuestionTemplate>
-  )
-}
+const OrderQuestion = ({ cost, text, items = [], onEdit: edit, onDelete: $delete }) => (
+  <QuestionTemplate cost={cost} text={text} onEdit={edit} onDelete={$delete}>
+    <ListGroup variant="flush">
+      {items
+        .sort((left, right) => left.orderIndex - right.orderIndex)
+        .map(({ id, text: optionText }, index) => (
+          <ListGroup.Item key={id}>
+            <span className="text-primary">{index + 1}.</span>
+            <span className="ml-1">{optionText}</span>
+          </ListGroup.Item>
+        ))}
+    </ListGroup>
+  </QuestionTemplate>
+)
 
 export default OrderQuestion
